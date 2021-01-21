b358beccec410e15896b05ce7f377fc2f7dd68a9
NayeemH <nayeemh67@gmail.com>
2021-01-21 12:41:17 +0600

    solve 2d array problem

---
+++ Day01/2dArray/2darray.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Nayeem Hasan on 21/1/21.
+//
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+    cout<<"hello";
+
+    return 0;
+}
---
+++ Day01/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[6][6],maxSum=-36985247;
+
+    for(int i=0;i<6;i++)
+        for(int j=0;j<6;j++)
+            cin>>a[i][j];
+
+    for(int i=0;i<6;i++){
+        for(int j=0;j<6;j++){
+            if(j+2<6 && i+2<6 ){
+                int sum = a[i][j] + a[i][j+1] + a[i][j+2] + a[i+1][j+1] + a[i+2][j] + a[i+2][j+1] +a[i+2][j+2];
+                if(sum>maxSum) maxSum=sum;
+            }
+        }
+    }
+
+    cout<<maxSum<<endl;
+
+}
